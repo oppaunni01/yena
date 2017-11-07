6e51cf2465a3c425e73e85cf0938942fd37ee537
oppaunni01 <33110905+oppaunni01@users.noreply.github.com>
2017-11-07 16:56:30 +0800

    Add files via upload

--- alin.py
+++ alin.py
@@ -1355,7 +1355,7 @@ def bot(op):
 #-----------------------------------------------------------)
 #----------------------ADMIN COMMAND------------------------------#
 
-            elif "Kick " in msg.text):
+            elif "Kick " in msg.text:
                 if msg.from_ in admin:
                     targets = []
                     key = eval(msg.contentMetadata["MENTION"])
