b03e035cb4f0607442b6c8f33cd9a998a2242f03
oppaunni01 <33110905+oppaunni01@users.noreply.github.com>
2018-01-21 20:54:57 +0800

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -43,7 +43,7 @@ class Channel:
         self.transport.path = self.channel_query_path
 
     def login(self):
-        result = self.client.issueChannelToken("1341209850")
+        result = self.client.issueChannelToken("1341209907")
 
         self.channel_access_token = result.channelAccessToken
         self.token = result.token
